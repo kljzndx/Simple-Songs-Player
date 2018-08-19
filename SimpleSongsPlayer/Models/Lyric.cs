e8a2ed793373720a1c55f1830fb7e935b8876617
李继龙 <kljzndx@outlook.com>
2018-08-19 15:07:06 +0800

    模型准备完成

---
+++ SimpleSongsPlayer/Models/Lyric.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GalaSoft.MvvmLight;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class Lyric : ObservableObject, IComparable<Lyric>
+    {
+        public Lyric(TimeSpan time, string content)
+        {
+            Time = time;
+            Content = content;
+        }
+
+        private bool isSelected;
+
+        public bool IsSelected
+        {
+            get => isSelected;
+            set => Set(ref isSelected, value);
+        }
+        public TimeSpan Time { get; }
+        public string Content { get; }
+
+        public int CompareTo(Lyric other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return Time.CompareTo(other.Time);
+        }
+    }
+}
