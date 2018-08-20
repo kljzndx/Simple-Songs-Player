2a544ba039ed9771b7790f4f6bed133f8250bedb
李继龙 <kljzndx@outlook.com>
2018-08-21 07:50:32 +0800

    文件读取器折腾完毕

---
+++ SimpleSongsPlayer.Operator/FileReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleSongsPlayer.Operator
+{
+    public static class FileReader
+    {
+        private static readonly Encoding gbkEncoding;
+
+        static FileReader()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            gbkEncoding = Encoding.GetEncoding("GBK");
+        }
+
+        public static async Task<string> ReadFileAsync(StorageFile file)
+        {
+            string result = String.Empty;
+
+            try
+            {
+                result = await FileIO.ReadTextAsync(file);
+            }
+            catch (Exception)
+            {
+                var buffer = await FileIO.ReadBufferAsync(file);
+                result = gbkEncoding.GetString(buffer.ToArray());
+            }
+
+            return result;
+        }
+    }
+}
