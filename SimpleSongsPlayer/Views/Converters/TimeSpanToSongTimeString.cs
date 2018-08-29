bb4646fe7808de9174fc031d1bb2a571005dd445
李继龙 <kljzndx@outlook.com>
2018-08-29 12:52:29 +0800

    完成歌曲项目模板设计

---
+++ SimpleSongsPlayer/Views/Converters/TimeSpanToSongTimeString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleSongsPlayer.Views.Converters
+{
+    public class TimeSpanToSongTimeString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            TimeSpan time = (TimeSpan) value;
+            if (time.Days > 0)
+                return $"{time.Days}:{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
+            else if (time.Hours > 0)
+                return $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
+            else
+                return $"{time.Minutes:D2}:{time.Seconds:D2}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleSongsPlayer/Views/ItemTemplates/SongItemTemplate.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SimpleSongsPlayer.DataModel;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace SimpleSongsPlayer.Views.ItemTemplates
+{
+    public sealed partial class SongItemTemplate : UserControl
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+            nameof(Source), typeof(Song), typeof(SongItemTemplate), new PropertyMetadata(null));
+
+        public SongItemTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        public Song Source
+        {
+            get => (Song) GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+    }
+}
