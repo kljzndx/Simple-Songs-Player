df514049ad976b92a2ea03c450fb0e5f7a1bec6e
李继龙 <kljzndx@outlook.com>
2018-10-04 00:06:16 +0800

    完善歌曲分类页面功能

---
+++ SimpleSongsPlayer/Models/SymbolIndexItemStyleSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class SymbolIndexItemStyleSelector : StyleSelector
+    {
+        private Style emptyStyle;
+        private Style haveStyle;
+
+        public SymbolIndexItemStyleSelector()
+        {
+            haveStyle = new Style {TargetType = typeof(GridViewItem)};
+            emptyStyle = new Style {TargetType = typeof(GridViewItem)};
+            Setter widthSetter = new Setter {Property = FrameworkElement.MinWidthProperty, Value = 60D};
+            Setter heightSetter = new Setter {Property = FrameworkElement.MinHeightProperty, Value = 50D};
+            Setter opacitySetter = new Setter
+            {
+                Property = UIElement.OpacityProperty,
+                Value = 0.3D
+            };
+
+            emptyStyle.Setters.Add(widthSetter);
+            emptyStyle.Setters.Add(heightSetter);
+            emptyStyle.Setters.Add(opacitySetter);
+
+            haveStyle.Setters.Add(widthSetter);
+            haveStyle.Setters.Add(heightSetter);
+        }
+
+        protected override Style SelectStyleCore(object item, DependencyObject container)
+        {
+            ICollectionViewGroup gp = item as ICollectionViewGroup;
+            SongsGroup sg = gp.Group as SongsGroup;
+
+            return sg.IsAny ? haveStyle : emptyStyle;
+        }
+    }
+}
