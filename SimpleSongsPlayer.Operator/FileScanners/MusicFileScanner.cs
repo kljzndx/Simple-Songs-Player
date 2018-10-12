77127ff347f1c5e2664b25068bcc3896ceab9e6d
李继龙 <kljzndx@outlook.com>
2018-10-12 09:38:21 +0800

    提升歌曲文件扫描性能

--- SimpleSongsPlayer.Operator/FileScanners/MusicFileScanner.cs
+++ SimpleSongsPlayer.Operator/FileScanners/MusicFileScanner.cs
@@ -1,9 +1,21 @@
-﻿namespace SimpleSongsPlayer.Operator.FileScanners
+﻿using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Search;
+
+namespace SimpleSongsPlayer.Operator.FileScanners
 {
     public class MusicFileScanner : FileScannerBase
     {
         public MusicFileScanner() : base("aac", "wav", "flac", "alac", "m4a", "mp3")
         {
+            string[] otherProperties =
+            {
+                SystemProperties.Music.Artist,
+                SystemProperties.Title,
+                SystemProperties.Music.AlbumTitle
+            };
+            QueryOptions.IndexerOption = IndexerOption.OnlyUseIndexerAndOptimizeForIndexedProperties;
+            QueryOptions.SetPropertyPrefetch(PropertyPrefetchOptions.MusicProperties, otherProperties);
         }
     }
 }
