e2f3b5f98f57d32b326b098743040691f6d4cb26
李继龙 <kljzndx@outlook.com>
2018-10-15 11:05:06 +0800

    创建播放列表操作通知器

---
+++ SimpleSongsPlayer/ViewModels/Events/PlayingListAdditionRequestedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleSongsPlayer.DataModel;
+
+namespace SimpleSongsPlayer.ViewModels.Events
+{
+    public class PlayingListAdditionRequestedEventArgs : EventArgs
+    {
+        public PlayingListAdditionRequestedEventArgs(IEnumerable<Song> songs)
+        {
+            Paths = songs.Select(s => s.Path).ToList();
+        }
+
+        public List<string> Paths { get; }
+    }
+}
---
+++ SimpleSongsPlayer/ViewModels/Events/PlayingListOperationNotifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleSongsPlayer.DataModel;
+
+namespace SimpleSongsPlayer.ViewModels.Events
+{
+    public static class PlayingListOperationNotifier
+    {
+        public static event EventHandler<PlayingListAdditionRequestedEventArgs> AdditionRequested;
+
+        public static void RequestAdd(IEnumerable<Song> songs)
+        {
+            AdditionRequested?.Invoke(null, new PlayingListAdditionRequestedEventArgs(songs));
+        }
+    }
+}
