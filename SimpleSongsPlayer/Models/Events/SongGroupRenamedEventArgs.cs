1b8b7c042d914128849b7e98828f2df3ba5b78f9
李继龙 <kljzndx@outlook.com>
2018-10-15 08:50:17 +0800

    增加重命名事件

---
+++ SimpleSongsPlayer/Models/Events/SongGroupRenamedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleSongsPlayer.Models.Events
+{
+    public class SongGroupRenamedEventArgs : EventArgs
+    {
+        public SongGroupRenamedEventArgs(string oldName, string newName)
+        {
+            OldName = oldName;
+            NewName = newName;
+        }
+
+        public string OldName { get; }
+        public string NewName { get; }
+    }
+}
--- SimpleSongsPlayer/Models/SongsGroup.cs
+++ SimpleSongsPlayer/Models/SongsGroup.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Windows.Foundation;
 using Windows.UI.Xaml.Media.Imaging;
 using SimpleSongsPlayer.DataModel;
+using SimpleSongsPlayer.Models.Events;
+using ObservableObject = GalaSoft.MvvmLight.ObservableObject;
 
 namespace SimpleSongsPlayer.Models
 {
-    public class SongsGroup
+    public class SongsGroup : ObservableObject
     {
+        private string _name;
+
         public SongsGroup(string name)
         {
-            Name = name;
+            _name = name;
             Items = new ObservableCollection<Song>();
         }
 
         public SongsGroup(string name, IEnumerable<Song> items)
         {
-            Name = name;
+            _name = name;
             Items = new ObservableCollection<Song>(items);
 
             var song = Items.LastOrDefault();
@@ -25,8 +30,23 @@ namespace SimpleSongsPlayer.Models
         }
 
         public bool IsAny => Items.Any();
-        public string Name { get; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                string oldName = _name;
+
+                Set(ref _name, value);
+
+                Renamed?.Invoke(this, new SongGroupRenamedEventArgs(oldName, value));
+            }
+        }
+
         public BitmapSource AlbumCover { get; set; }
         public ObservableCollection<Song> Items { get; }
+
+        public event TypedEventHandler<SongsGroup, SongGroupRenamedEventArgs> Renamed;
     }
 }
