adbfa2b68a4b25eba954311f31da595269449e62
李继龙 <kljzndx@outlook.com>
2018-10-19 09:51:57 +0800

    Fixed bug

--- SimpleSongsPlayer/ViewModels/SongViewModels/SongsViewModelLocator.cs
+++ SimpleSongsPlayer/ViewModels/SongViewModels/SongsViewModelLocator.cs
@@ -10,11 +10,13 @@ namespace SimpleSongsPlayer.ViewModels.SongViewModels
             SimpleIoc.Default.Register<SongArtistsViewModel>();
             SimpleIoc.Default.Register<SongAlbumsViewModel>();
             SimpleIoc.Default.Register<SongsFoldersViewModel>();
+            SimpleIoc.Default.Register<PlayingListViewModel>();
         }
 
         public SongsViewModel Songs => SimpleIoc.Default.GetInstance<SongsViewModel>();
         public SongArtistsViewModel SongArtists => SimpleIoc.Default.GetInstance<SongArtistsViewModel>();
         public SongAlbumsViewModel SongAlbums => SimpleIoc.Default.GetInstance<SongAlbumsViewModel>();
         public SongsFoldersViewModel SongFolders => SimpleIoc.Default.GetInstance<SongsFoldersViewModel>();
+        public PlayingListViewModel PlayingList => SimpleIoc.Default.GetInstance<PlayingListViewModel>();
     }
 }
