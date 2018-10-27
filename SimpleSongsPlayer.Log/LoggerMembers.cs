9b71865b17f5cc6d895cdf983b09c5cd1de43da5
李继龙 <kljzndx@outlook.com>
2018-10-27 21:02:46 +0800

    新增日志功能

---
+++ SimpleSongsPlayer.Log/LoggerMembers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using NLog;
+using NLog.Config;
+
+namespace SimpleSongsPlayer.Log
+{
+    public static class LoggerMembers
+    {
+        public static Logger PagesLogger { get; }
+
+        static LoggerMembers()
+        {
+            LogManager.Configuration.Variables["tempFolderPath"] = ApplicationData.Current.TemporaryFolder.Path;
+            PagesLogger = LogManager.GetLogger("pages");
+        }
+        
+    }
+}
