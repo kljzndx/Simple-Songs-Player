f361a4c28027dcddb074980cde475d6dea5742ab
李继龙 <kljzndx@outlook.com>
2018-10-28 17:32:47 +0800

    重命名Logger

--- SimpleSongsPlayer.Log/LoggerMembers.cs
+++ SimpleSongsPlayer.Log/LoggerMembers.cs
@@ -6,6 +6,7 @@ namespace SimpleSongsPlayer.Log
     public static class LoggerMembers
     {
         public static Logger PagesLogger { get; }
+        public static Logger PlayerLogger { get; }
 
         static LoggerMembers()
         {
@@ -13,6 +14,7 @@ namespace SimpleSongsPlayer.Log
             LogManager.Configuration.Variables["tempFolderPath"] = ApplicationData.Current.TemporaryFolder.Path;
 
             PagesLogger = LogManager.GetLogger("pages");
+            PlayerLogger = LogManager.GetLogger("player");
         }
         
     }
