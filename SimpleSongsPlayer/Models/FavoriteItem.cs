eb6f26d082940de948ca3b7ee475afc88166b7a2
李继龙 <kljzndx@outlook.com>
2018-11-10 16:31:04 +0800

    增加收藏面板

---
+++ SimpleSongsPlayer/Models/FavoriteItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Media.Imaging;
+using SimpleSongsPlayer.DataModel;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class FavoriteItem
+    {
+        private FavoriteItem(PlayingListBlock block, StorageItemThumbnail thumbnail)
+        {
+            Name = block.Name;
+            Count = block.Count;
+            BitmapImage image = new BitmapImage();
+            image.SetSource(thumbnail);
+            Cover = image;
+        }
+
+        public string Name { get; private set; }
+        public int Count { get; private set; }
+        public BitmapSource Cover { get; private set; }
+
+        public static async Task<FavoriteItem> FromPlayingListBlock(PlayingListBlock block)
+        {
+            var paths = await block.GetPaths();
+            var coverSource = await (await StorageFile.GetFileFromPathAsync(paths.First())).GetThumbnailAsync(ThumbnailMode.MusicView);
+            return new FavoriteItem(block, coverSource);
+        }
+    }
+}
---
+++ SimpleSongsPlayer/Views/Controllers/FavoritesDialog.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SimpleSongsPlayer.Models;
+using SimpleSongsPlayer.Operator;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace SimpleSongsPlayer.Views.Controllers
+{
+    public sealed partial class FavoritesDialog : ContentDialog
+    {
+        private PlayingListManager manager;
+
+        public FavoritesDialog()
+        {
+            this.InitializeComponent();
+            this.Opened += FavoritesDialog_Opened;
+        }
+
+        public event TypedEventHandler<FavoritesDialog, EventArgs> RequestAdd;
+        public event ItemClickEventHandler ItemClick;
+
+        private async void FavoritesDialog_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            if (manager is null)
+                manager = await PlayingListManager.GetManager();
+
+            List<FavoriteItem> favoriteItems = new List<FavoriteItem>();
+            var blocks = manager.GetBlocks();
+            foreach (var block in blocks)
+                favoriteItems.Add(await FavoriteItem.FromPlayingListBlock(block));
+
+            Main_ListView.ItemsSource = favoriteItems;
+        }
+
+        private void Main_ListView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Hide();
+            ItemClick?.Invoke(sender, e);
+        }
+
+        private void Add_ListViewItem_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Hide();
+            RequestAdd?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
