f8345db47af6357a83de1b417bf6b863b135f002
李继龙 <kljzndx@outlook.com>
2018-11-15 09:56:38 +0800

    添加单元测试项目

---
+++ SimpleSongsPlayer.Test/DAL/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.DAL;
+
+namespace SimpleSongsPlayer.Test.DAL
+{
+    [TestClass]
+    public class ContextHelper
+    {
+        [TestMethod]
+        public void Add()
+        {
+            MusicFile file = new MusicFile
+            {
+                LibraryFolder = "Music", Title = "Journey", Duration = TimeSpan.FromMinutes(3),
+                Path = "C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3"
+            };
+
+            ContextHelper<FilesContext, MusicFile>.Add(file);
+
+            Assert.IsNotNull(ContextHelper<FilesContext, MusicFile>.Find("C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3"));
+        }
+    }
+}
