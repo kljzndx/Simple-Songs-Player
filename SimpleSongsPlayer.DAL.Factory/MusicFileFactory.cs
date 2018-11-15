ffc2d2135e820b9963631351906a43fa901308ed
李继龙 <kljzndx@outlook.com>
2018-11-15 23:41:13 +0800

    添加DAL Factory类库

---
+++ SimpleSongsPlayer.DAL.Factory/MusicFileFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleSongsPlayer.DAL.Factory
+{
+    public static class MusicFileFactory
+    {
+        public static async Task<MusicFile> FromStorageFile(string libraryFolder, StorageFile file)
+        {
+            var properties = await file.Properties.GetMusicPropertiesAsync();
+
+            return new MusicFile
+            {
+                Title = String.IsNullOrWhiteSpace(properties.Title) ? file.DisplayName : properties.Title,
+                Artist = properties.Artist,
+                Album = properties.Album,
+                Path = file.Path,
+                Duration = properties.Duration,
+                LibraryFolder = libraryFolder
+            };
+        }
+    }
+}
