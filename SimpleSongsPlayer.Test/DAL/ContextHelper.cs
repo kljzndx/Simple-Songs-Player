8c0ad0927fee73fec19f8977db3ed963a48d0dbd
李继龙 <kljzndx@outlook.com>
2018-11-16 10:36:01 +0800

    Fixed bugs

--- SimpleSongsPlayer.Test/DAL/ContextHelper.cs
+++ SimpleSongsPlayer.Test/DAL/ContextHelper.cs
@@ -9,34 +9,38 @@ namespace SimpleSongsPlayer.Test.DAL
     {
         private readonly ContextHelper<FilesContext, MusicFile> helper = new ContextHelper<FilesContext, MusicFile>();
 
-        public MusicFile Add()
+        public MusicFile GetFile()
         {
-            var queryResult = helper.Find("C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3");
-            if (queryResult != null)
-                return queryResult;
-
             MusicFile file = new MusicFile
             {
                 LibraryFolder = "Music", Title = "Journey", Duration = TimeSpan.FromMinutes(3),
                 Path = "C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3"
             };
 
-            helper.Add(file);
             return file;
         }
 
         [TestMethod]
         public void Test_Add()
         {
-            Add();
             var queryResult = helper.Find("C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3");
+            if (queryResult != null)
+            {
+                Assert.IsNotNull(queryResult);
+                return;
+            }
+
+            var file = GetFile();
+
+            helper.Add(file);
+            queryResult = helper.Find("C:\\Users\\kljzn\\Music\\Capo Productions - Journey.mp3");
             Assert.IsNotNull(queryResult);
         }
 
         [TestMethod]
         public void Test_Remove()
         {
-            var obj = Add();
+            var obj = GetFile();
 
             helper.Remove(obj);
 
