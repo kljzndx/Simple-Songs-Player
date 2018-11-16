d72fa478ef0e359ef9a4e4144d77c15747180851
李继龙 <kljzndx@outlook.com>
2018-11-16 16:54:08 +0800

    Fixed bugs

--- SimpleSongsPlayer.Test/Service/MusicLibraryServiceTest.cs
+++ SimpleSongsPlayer.Test/Service/MusicLibraryServiceTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.DAL;
+using SimpleSongsPlayer.DAL.Factory;
 using SimpleSongsPlayer.Service;
 
 namespace SimpleSongsPlayer.Test.Service
@@ -14,7 +16,7 @@ namespace SimpleSongsPlayer.Test.Service
         [TestMethod]
         public async Task Scan()
         {
-            var service = await MusicLibraryService.GetService();
+            var service = await MusicLibraryService<MusicFile, MusicFileFactory>.GetService("mp3", "aac", "wav", "flac", "alac", "m4a");
             await service.ScanFiles();
             var files = service.GetFiles();
             Assert.IsTrue(files.Any());
