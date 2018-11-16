339c547a17e390002780f4ab5072488b1cb65abe
李继龙 <kljzndx@outlook.com>
2018-11-16 10:40:38 +0800

    添加服务测试类

---
+++ SimpleSongsPlayer.Test/Service/MusicLibraryServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.Service;
+
+namespace SimpleSongsPlayer.Test.Service
+{
+    [TestClass]
+    public class MusicLibraryServiceTest
+    {
+        [TestMethod]
+        public async Task Scan()
+        {
+            var service = await MusicLibraryService.GetService();
+            await service.ScanFiles();
+            var files = service.GetFiles();
+            Assert.IsTrue(files.Any());
+        }
+    }
+}
