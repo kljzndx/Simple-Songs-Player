f607200f852e2e959c6319831e47280b47a8c811
李继龙 <kljzndx@outlook.com>
2018-11-17 19:03:10 +0800

    完成对 LoggerService 的测试

---
+++ SimpleSongsPlayer.Test/Service/LoggerServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.Service;
+using SimpleSongsPlayer.Service.Models;
+
+namespace SimpleSongsPlayer.Test.Service
+{
+    [TestClass]
+    public class LoggerServiceTest
+    {
+        [TestMethod]
+        public void GetLogger()
+        {
+            var logger = LoggerService.GetLogger(LoggerMembers.UnitTest);
+            Assert.IsNotNull(logger);
+        }
+
+        public async Task ReadLogs()
+        {
+            var logger = LoggerService.GetLogger(LoggerMembers.UnitTest);
+            logger.Info("测试1");
+            logger.Info("测试2");
+            logger.Info("测试3");
+            logger.Info("测试4");
+            logger.Info("测试5");
+            var text = await LoggerService.ReadLogs(5);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(text));
+        }
+    }
+}
