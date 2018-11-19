c05667d48886508710c32668459d6fe4a93f05ee
李继龙 <kljzndx@outlook.com>
2018-11-19 19:01:42 +0800

    添加测试代码

---
+++ SimpleSongsPlayer.Test/Service/UserFavoriteServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.DAL;
+using SimpleSongsPlayer.DAL.Factory;
+using SimpleSongsPlayer.Service;
+
+namespace SimpleSongsPlayer.Test.Service
+{
+    [TestClass]
+    public class UserFavoriteServiceTest
+    {
+        private static readonly UserFavoriteService FavoriteService = UserFavoriteService.GetService();
+
+        static UserFavoriteServiceTest()
+        {
+            // MusicLibraryService<MusicFile, MusicFileFactory>.SetupFileTypeFilter("mp3", "aac", "wav", "flac", "alac", "m4a");
+        }
+
+        [TestMethod]
+        public async Task AddRange()
+        {
+            var libraryService = await MusicLibraryService<MusicFile, MusicFileFactory>.GetService();
+            List<MusicFile> allFiles = libraryService.GetFiles();
+            FavoriteService.AddRange("test", allFiles);
+            List<UserFavorite> f = FavoriteService.GetFiles();
+            Assert.IsTrue(f.Count > 30);
+        }
+
+        [TestMethod]
+        public async Task RemoveRange()
+        {
+            var libraryService = await MusicLibraryService<MusicFile, MusicFileFactory>.GetService();
+            List<MusicFile> allFiles = libraryService.GetFiles();
+            FavoriteService.RemoveRange(allFiles);
+            List<UserFavorite> f = FavoriteService.GetFiles();
+            Assert.IsFalse(f.Any());
+        }
+    }
+}
