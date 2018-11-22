82d5e4b2345ec32007d4f7d24e29c6409921b142
李继龙 <kljzndx@outlook.com>
2018-11-22 11:15:04 +0800

    删除没用的代码

--- SimpleSongsPlayer.Test/UnitTestApp.xaml.cs
+++ SimpleSongsPlayer.Test/UnitTestApp.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Microsoft.EntityFrameworkCore;
-using SimpleSongsPlayer.DAL;
 
 namespace SimpleSongsPlayer.Test
 {
@@ -32,7 +20,6 @@ namespace SimpleSongsPlayer.Test
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
-
         }
 
         /// <summary>
