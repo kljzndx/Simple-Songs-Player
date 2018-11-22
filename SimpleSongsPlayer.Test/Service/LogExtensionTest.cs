b23e27a668cf305829605c6c8d22b8e1f21a70aa
李继龙 <kljzndx@outlook.com>
2018-11-22 11:16:06 +0800

    添加 日志扩展类 测试代码

---
+++ SimpleSongsPlayer.Test/Service/LogExtensionTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSongsPlayer.Service;
+using SimpleSongsPlayer.Service.Models;
+
+namespace SimpleSongsPlayer.Test.Service
+{
+    [TestClass]
+    public class LogExtensionTest
+    {
+        static LogExtensionTest()
+        {
+            LogExtension.SetUpAssembly(typeof(App).GetTypeInfo().Assembly, LoggerMembers.UnitTest);
+        }
+
+        [TestMethod]
+        public async Task LogByObjectTest()
+        {
+            for (int i = 0; i < 10; i++)
+                this.LogByObject($"测试 LogExtension 已测试 {i} 次");
+
+            string str = await LoggerService.ReadLogs(LoggerMembers.UnitTest, 30);
+            Assert.IsTrue(str.Contains("测试 LogExtension"));
+        }
+    }
+}
