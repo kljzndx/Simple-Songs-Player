2921eafdfeb337ea6d3ac1cac7f36f3b6d75cfc0
李继龙 <kljzndx@outlook.com>
2018-11-24 21:22:51 +0800

    添加音乐文件组模型

---
+++ SimpleSongsPlayer/Models/MusicFileGroup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Media.Imaging;
+using GalaSoft.MvvmLight;
+using SimpleSongsPlayer.Models.DTO;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicFileGroup : ObservableObject
+    {
+        private string name;
+
+        public MusicFileGroup(string name, IList<MusicFileDTO> items)
+        {
+            this.name = name;
+            Items = items;
+        }
+
+        public MusicFileGroup(string name, IList<MusicFileDTO> items, BitmapSource cover) : this(name, items)
+        {
+            Cover = cover;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+
+        public BitmapSource Cover { get; }
+        public IList<MusicFileDTO> Items { get; }
+    }
+}
