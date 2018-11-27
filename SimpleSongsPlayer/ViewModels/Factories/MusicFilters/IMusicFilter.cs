ef4ba1fb694a3c9860aad86df206b66a15d08d93
李继龙 <kljzndx@outlook.com>
2018-11-27 19:23:11 +0800

    添加两个工厂

---
+++ SimpleSongsPlayer/ViewModels/Factories/MusicFilters/IMusicFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SimpleSongsPlayer.Models;
+using SimpleSongsPlayer.Models.DTO;
+
+namespace SimpleSongsPlayer.ViewModels.Factories.MusicFilters
+{
+    public interface IMusicFilter
+    {
+        void Filter(IEnumerable<MusicFileDTO> source, ObservableCollection<MusicFileDynamic> target);
+    }
+}
---
+++ SimpleSongsPlayer/ViewModels/Factories/MusicGroupers/IMusicGrouper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SimpleSongsPlayer.DAL;
+using SimpleSongsPlayer.Models;
+
+namespace SimpleSongsPlayer.ViewModels.Factories.MusicGroupers
+{
+    public interface IMusicGrouper
+    {
+        void Group(IEnumerable<MusicFile> source, ObservableCollection<MusicFileGroup> target);
+    }
+}
