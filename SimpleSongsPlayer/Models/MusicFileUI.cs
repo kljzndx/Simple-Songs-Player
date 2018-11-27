91f57fcc6035d18a2c863db58548f0060f689360
李继龙 <kljzndx@outlook.com>
2018-11-27 08:57:16 +0800

    为项目模板创建专门的类

---
+++ SimpleSongsPlayer/Models/MusicFileUI.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+using SimpleSongsPlayer.Models.DTO;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicFileUI : ObservableObject
+    {
+        private bool isSelected;
+
+        public MusicFileUI(MusicFileDTO original)
+        {
+            Original = original;
+        }
+
+        public MusicFileDTO Original { get; }
+        
+        public bool IsSelected
+        {
+            get => isSelected;
+            set => Set(ref isSelected, value);
+        }
+    }
+}
