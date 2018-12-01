4f860d893417737df0d2873634c0059cab70005c
李继龙 <kljzndx@outlook.com>
2018-12-01 08:10:39 +0800

    初步完成后退功能

--- SimpleSongsPlayer/Views/FrameworkPage.xaml.cs
+++ SimpleSongsPlayer/Views/FrameworkPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 using SimpleSongsPlayer.Service;
 using SimpleSongsPlayer.ViewModels;
 using SimpleSongsPlayer.ViewModels.Factories;
@@ -18,10 +19,33 @@ namespace SimpleSongsPlayer.Views
     /// </summary>
     public sealed partial class FrameworkPage : Page
     {
+        private static readonly Type ClassifyPageType = typeof(MusicClassifyPage);
+
+        private readonly SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+
         public FrameworkPage()
         {
             this.InitializeComponent();
-            Main_Frame.Navigate(typeof(MusicClassifyPage));
+            Main_Frame.Navigate(ClassifyPageType);
+            systemNavigationManager.BackRequested += SystemNavigationManager_BackRequested;
+        }
+
+        private void Main_Frame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            systemNavigationManager.AppViewBackButtonVisibility = e.SourcePageType != ClassifyPageType
+                ? AppViewBackButtonVisibility.Visible
+                : AppViewBackButtonVisibility.Disabled;
+        }
+
+        private void SystemNavigationManager_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (Main_Frame.CanGoBack)
+            {
+                if (e != null)
+                    e.Handled = true;
+
+                Main_Frame.GoBack();
+            }
         }
     }
 }
