1ad55839aa90bb401ed738a6ca9cdb62e4505d3d
李继龙 <kljzndx@outlook.com>
2018-12-02 07:24:35 +0800

    给页面标题特性增加缓存支持

--- SimpleSongsPlayer/Models/Attributes/PageTitleAttribute.cs
+++ SimpleSongsPlayer/Models/Attributes/PageTitleAttribute.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using Windows.ApplicationModel.Resources;
 
 namespace SimpleSongsPlayer.Models.Attributes
 {
     public class PageTitleAttribute : Attribute
     {
+        private static readonly Dictionary<string, string> AllTitles = new Dictionary<string, string>();
+
         public PageTitleAttribute(string stringResourceName, string stringResourceKey)
         {
             StringResourceName = stringResourceName;
@@ -20,11 +23,18 @@ namespace SimpleSongsPlayer.Models.Attributes
 
         public string GetTitle()
         {
-            string str = ResourceLoader.GetForCurrentView(StringResourceName).GetString(StringResourceKey);
-            if (String.IsNullOrWhiteSpace(str))
-                throw new NullReferenceException("No found title");
+            var key = String.Format("{0}/{1}", StringResourceName, StringResourceKey);
 
-            return str;
+            if (!AllTitles.ContainsKey(key))
+            {
+                string str = ResourceLoader.GetForCurrentView(StringResourceName).GetString(StringResourceKey);
+                if (String.IsNullOrWhiteSpace(str))
+                    throw new NullReferenceException("No found title");
+
+                AllTitles.Add(key, str);
+            }
+
+            return AllTitles[key];
         }
     }
 }
