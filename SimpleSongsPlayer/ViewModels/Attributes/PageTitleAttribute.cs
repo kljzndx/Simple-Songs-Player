39ad4a1b84a52fe0bb0c8c652ab9900f60622e70
李继龙 <kljzndx@outlook.com>
2018-12-02 15:55:35 +0800

    转移页面标题特性

--- SimpleSongsPlayer/ViewModels/Attributes/PageTitleAttribute.cs
+++ SimpleSongsPlayer/ViewModels/Attributes/PageTitleAttribute.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Windows.ApplicationModel.Resources;
 
-namespace SimpleSongsPlayer.Models.Attributes
+namespace SimpleSongsPlayer.ViewModels.Attributes
 {
     public class PageTitleAttribute : Attribute
     {
