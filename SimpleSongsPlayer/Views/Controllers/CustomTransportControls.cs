9209f920b249c869a0ced3ddd6e5f029a17cb448
李继龙 <kljzndx@outlook.com>
2018-12-02 15:48:25 +0800

    增加三个事件

--- SimpleSongsPlayer/Views/Controllers/CustomTransportControls.cs
+++ SimpleSongsPlayer/Views/Controllers/CustomTransportControls.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
 using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
@@ -16,9 +19,46 @@ namespace SimpleSongsPlayer.Views.Controllers
 {
     public sealed class CustomTransportControls : MediaTransportControls
     {
+        public static readonly DependencyProperty RepeatMode_SelectedIDProperty = DependencyProperty.Register(
+            nameof(RepeatMode_SelectedID), typeof(int), typeof(CustomTransportControls), new PropertyMetadata(-1));
+
         public CustomTransportControls()
         {
             this.DefaultStyleKey = typeof(CustomTransportControls);
+        }
+
+        public int RepeatMode_SelectedID
+        {
+            get => (int) GetValue(RepeatMode_SelectedIDProperty);
+            set => SetValue(RepeatMode_SelectedIDProperty, value);
+        }
+
+        public event EventHandler<object> RewindButtonClick;
+        public event EventHandler<object> FastForwardButtonClick;
+        public event TypedEventHandler<CustomTransportControls, KeyValuePair<int, string>> RepeatModeSelectionChanged;
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            {
+                var repeatModeListBox = (ListBox) GetTemplateChild("RepeatMode_ListBox");
+                repeatModeListBox.SelectionChanged += (s, e) =>
+                {
+                    var sender = (ListBox) s;
+                    RepeatModeSelectionChanged?.Invoke(this, new KeyValuePair<int, string>(sender.SelectedIndex, ((ListBoxItem)sender.SelectedItem).Content.ToString()));
+                };
+            }
+
+            {
+                var rewindButton = (AppBarButton) GetTemplateChild("Rewind_Button");
+                rewindButton.Click += (s, e) => RewindButtonClick?.Invoke(this, null);
+            }
+
+            {
+                var fastForwardButton = (AppBarButton) GetTemplateChild("FastForward_Button");
+                fastForwardButton.Click += (s, e) => FastForwardButtonClick?.Invoke(this, null);
+            }
         }
     }
 }
