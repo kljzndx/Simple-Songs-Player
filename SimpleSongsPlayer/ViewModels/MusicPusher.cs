5c0f3433297fbbf2aa9617e8ae422ccfef7a9bc3
李继龙 <kljzndx@outlook.com>
2018-12-03 19:22:42 +0800

    为音乐推送器部署日志

--- SimpleSongsPlayer/ViewModels/MusicPusher.cs
+++ SimpleSongsPlayer/ViewModels/MusicPusher.cs
@@ -1,28 +1,42 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Media.Playback;
 using SimpleSongsPlayer.Models.DTO;
+using SimpleSongsPlayer.Service;
 
 namespace SimpleSongsPlayer.ViewModels
 {
     public static class MusicPusher
     {
+        private static readonly Type CurrentType = typeof(MusicPusher);
+
         private static void AddItem(MediaPlaybackList target, MediaPlaybackItem item)
         {
             if (!target.Items.Contains(item))
+            {
+                CurrentType.LogByType("添加播放项至播放列表");
                 target.Items.Add(item);
+            }
         }
 
         public static async Task Push(MusicFileDTO music)
         {
+            CurrentType.LogByType("获取播放项");
             var playbackItem = await music.GetPlaybackItem();
-
+            
             if (App.MediaPlayer.Source is MediaPlaybackList mpl)
             {
-                if (mpl.Items.Contains(playbackItem))
-                    mpl.Items.Remove(playbackItem);
+                if (mpl.CurrentItem.Equals(playbackItem))
+                    return;
 
+                if (mpl.Items.Contains(playbackItem))
+                {
+                    CurrentType.LogByType("移除列表内相同播放项");
+                    mpl.Items.Remove(playbackItem);
+                }
+
+                CurrentType.LogByType("将播放项插入至最顶端，并同步移动磁头");
                 mpl.Items.Insert(0, playbackItem);
                 mpl.MoveTo(0);
             }
@@ -32,15 +46,17 @@ namespace SimpleSongsPlayer.ViewModels
 
         public static async Task Push(IEnumerable<MusicFileDTO> items)
         {
+            CurrentType.LogByType("正在提取播放项");
             List<MediaPlaybackItem> list = new List<MediaPlaybackItem>();
             foreach (var file in items)
                 list.Add(await file.GetPlaybackItem());
-
+            
             Push(list);
         }
 
         public static async Task PushToNext(MusicFileDTO file)
         {
+            CurrentType.LogByType("获取播放项");
             var mpi = await file.GetPlaybackItem();
 
             if (App.MediaPlayer.Source is MediaPlaybackList mpl)
@@ -49,8 +65,12 @@ namespace SimpleSongsPlayer.ViewModels
                     return;
 
                 if (mpl.Items.Contains(mpi))
+                {
+                    CurrentType.LogByType("移除列表内相同播放项");
                     mpl.Items.Remove(mpi);
+                }
 
+                CurrentType.LogByType("将播放项添加至正在播放音乐的下面");
                 uint id = mpl.CurrentItemIndex + 1;
                 mpl.Items.Insert((int) id, mpi);
             }
@@ -62,6 +82,7 @@ namespace SimpleSongsPlayer.ViewModels
 
         public static async Task Append(IEnumerable<MusicFileDTO> files)
         {
+            CurrentType.LogByType("正在提取播放项");
             List<MediaPlaybackItem> items = new List<MediaPlaybackItem>();
             foreach (var file in files)
                 items.Add(await file.GetPlaybackItem());
@@ -75,9 +96,12 @@ namespace SimpleSongsPlayer.ViewModels
 
         private static void Push(IEnumerable<MediaPlaybackItem> items)
         {
+            CurrentType.LogByType("正在创建播放列表");
             var mpl = new MediaPlaybackList();
             foreach (var item in items)
                 AddItem(mpl, item);
+
+            CurrentType.LogByType("正在配置播放模块");
             App.MediaPlayer.Source = mpl;
         }
     }
