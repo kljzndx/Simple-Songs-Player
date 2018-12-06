e391609ff6d7014f413ae78cbb3f1f9f0579d1fd
李继龙 <kljzndx@outlook.com>
2018-12-06 13:09:20 +0800

    添加单一分组器

---
+++ SimpleSongsPlayer/ViewModels/Factories/MusicGroupers/SingleGrouper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SimpleSongsPlayer.Models;
+using SimpleSongsPlayer.Models.DTO;
+
+namespace SimpleSongsPlayer.ViewModels.Factories.MusicGroupers
+{
+    public class SingleGrouper : IMusicGrouper
+    {
+        public Task<IEnumerable<MusicFileGroup>> Group(IEnumerable<MusicFileDTO> source)
+        {
+            return Task.FromResult<IEnumerable<MusicFileGroup>>(new[] {new MusicFileGroup(String.Empty, source)});
+        }
+    }
+}
