7d815c4146c2d07266d7ef48b8417eb70869218e
李继龙 <kljzndx@outlook.com>
2018-12-06 12:43:39 +0800

    添加 MusicFileGroupDynamic

---
+++ SimpleSongsPlayer/Models/MusicFileGroupDynamic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicFileGroupDynamic : ObservableObject
+    {
+        public MusicFileGroupDynamic(MusicFileGroup group)
+        {
+            Name = group.Name;
+            Items = new ObservableCollection<MusicFileDynamic>();
+            foreach (var item in group.Items)
+                Items.Add(new MusicFileDynamic(item));
+        }
+
+        public string Name { get; }
+        public ObservableCollection<MusicFileDynamic> Items { get; }
+
+        public void Join(MusicFileGroup data)
+        {
+            foreach (var dto in data.Items)
+                Items.Add(new MusicFileDynamic(dto));
+        }
+    }
+}
