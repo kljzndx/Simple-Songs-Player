18dd9b07564f68a6c8683bb46614c794a9a3de65
李继龙 <kljzndx@outlook.com>
2018-12-06 23:45:26 +0800

    创建两个组样式和一个选择器

---
+++ SimpleSongsPlayer/ViewModels/StyleSelectors/MusicGroupDynamicStyleSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using SimpleSongsPlayer.Models;
+
+namespace SimpleSongsPlayer.ViewModels.StyleSelectors
+{
+    public class MusicGroupDynamicStyleSelector : GroupStyleSelector
+    {
+        public GroupStyle Normal { get; set; }
+        public GroupStyle NoName { get; set; }
+
+        protected override GroupStyle SelectGroupStyleCore(object group, uint level)
+        {
+            var mg = group as ICollectionViewGroup;
+            if (mg is null || String.IsNullOrWhiteSpace(((MusicFileGroupDynamic) mg.Group).Name))
+                return NoName;
+
+            return Normal;
+        }
+    }
+}
