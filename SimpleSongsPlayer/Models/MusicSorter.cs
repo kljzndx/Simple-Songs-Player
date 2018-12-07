fc6a5625c577b2793514f8297de3725374cd2ec4
李继龙 <kljzndx@outlook.com>
2018-12-07 17:10:11 +0800

    添加音乐排序器模型

---
+++ SimpleSongsPlayer/Models/MusicSorter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.ApplicationModel.Resources;
+using Windows.Globalization.Collation;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicSorter
+    {
+        private static readonly ResourceLoader ListStringResource = ResourceLoader.GetForCurrentView("MusicListPage");
+
+        public MusicSorter(string resourceKey, Func<MusicFileDynamic, CharacterGroupings, IComparable> keySelector)
+        {
+            Name = ListStringResource.GetString(resourceKey);
+            KeySelector = keySelector;
+        }
+
+        public string Name { get; }
+        public Func<MusicFileDynamic, CharacterGroupings, IComparable> KeySelector { get; }
+    }
+}
