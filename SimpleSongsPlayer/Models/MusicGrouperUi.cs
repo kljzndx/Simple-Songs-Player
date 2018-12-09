9f51e785d0a47d664d7cb57140495bc543e2f185
李继龙 <kljzndx@outlook.com>
2018-12-09 08:07:33 +0800

    优化分组器类

--- SimpleSongsPlayer/Models/MusicGrouperUi.cs
+++ SimpleSongsPlayer/Models/MusicGrouperUi.cs
@@ -1,12 +1,15 @@
-﻿using SimpleSongsPlayer.ViewModels.Factories.MusicGroupers;
+﻿using Windows.ApplicationModel.Resources;
+using SimpleSongsPlayer.ViewModels.Factories.MusicGroupers;
 
 namespace SimpleSongsPlayer.Models
 {
     public class MusicGrouperUi
     {
-        public MusicGrouperUi(string name, IMusicGrouper grouper)
+        private static readonly ResourceLoader ListStringResource = ResourceLoader.GetForCurrentView("MusicListPage");
+
+        public MusicGrouperUi(string resourceKey, IMusicGrouper grouper)
         {
-            Name = name;
+            Name = ListStringResource.GetString(resourceKey);
             Grouper = grouper;
         }
 
