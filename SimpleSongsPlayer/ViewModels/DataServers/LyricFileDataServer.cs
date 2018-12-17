9aae4c0334bcf58a5aca964bae9be8b900b99e93
李继龙 <kljzndx@outlook.com>
2018-12-17 16:50:14 +0800

    添加俩事件

--- SimpleSongsPlayer/ViewModels/DataServers/LyricFileDataServer.cs
+++ SimpleSongsPlayer/ViewModels/DataServers/LyricFileDataServer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,13 +21,19 @@ namespace SimpleSongsPlayer.ViewModels.DataServers
         }
 
         public ObservableCollection<LyricFileDTO> Data { get; } = new ObservableCollection<LyricFileDTO>();
+        public event EventHandler<IEnumerable<LyricFileDTO>> FilesAdded;
+        public event EventHandler<IEnumerable<LyricFileDTO>> FilesRemoved;
 
         public async Task Init()
         {
+            if (service != null)
+                return;
+
             service = await MusicLibraryService<LyricFile, LyricFileFactory>.GetService();
             var files = await service.GetFiles();
             foreach (var file in files)
                 Data.Add(new LyricFileDTO(file));
+            FilesAdded?.Invoke(this, Data);
 
             service.FilesAdded += Service_FilesAdded;
             service.FilesRemoved += Service_FilesRemoved;
@@ -42,14 +49,29 @@ namespace SimpleSongsPlayer.ViewModels.DataServers
 
         private void Service_FilesAdded(object sender, IEnumerable<LyricFile> e)
         {
+            List<LyricFileDTO> options = new List<LyricFileDTO>();
+
             foreach (var file in e)
-                Data.Add(new LyricFileDTO(file));
+            {
+                var dto = new LyricFileDTO(file);
+                options.Add(dto);
+                Data.Add(dto);
+            }
+            FilesAdded?.Invoke(this, options);
         }
 
         private void Service_FilesRemoved(object sender, IEnumerable<LyricFile> e)
         {
+            List<LyricFileDTO> options = new List<LyricFileDTO>();
+
             foreach (var file in e)
-                Data.Remove(Data.First(d => d.FilePath == file.Path));
+            {
+                var dto = Data.First(d => d.FilePath == file.Path);
+                options.Add(dto);
+                Data.Remove(dto);
+            }
+
+            FilesRemoved?.Invoke(this, options);
         }
     }
 }
