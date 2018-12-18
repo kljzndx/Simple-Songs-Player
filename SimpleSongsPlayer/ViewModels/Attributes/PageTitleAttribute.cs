fd66151ec662952b2a0fea7c18e025840ede4c1f
李继龙 <kljzndx@outlook.com>
2018-12-18 09:41:34 +0800

    去除毫无意义的缓存

--- SimpleSongsPlayer/ViewModels/Attributes/PageTitleAttribute.cs
+++ SimpleSongsPlayer/ViewModels/Attributes/PageTitleAttribute.cs
@@ -6,8 +6,6 @@ namespace SimpleSongsPlayer.ViewModels.Attributes
 {
     public class PageTitleAttribute : Attribute
     {
-        private static readonly Dictionary<string, string> AllTitles = new Dictionary<string, string>();
-
         public PageTitleAttribute(string stringResourceName, string stringResourceKey)
         {
             StringResourceName = stringResourceName;
@@ -23,18 +21,11 @@ namespace SimpleSongsPlayer.ViewModels.Attributes
 
         public string GetTitle()
         {
-            var key = String.Format("{0}/{1}", StringResourceName, StringResourceKey);
+            string str = ResourceLoader.GetForCurrentView(StringResourceName).GetString(StringResourceKey);
+            if (String.IsNullOrWhiteSpace(str))
+                throw new NullReferenceException("No found title");
 
-            if (!AllTitles.ContainsKey(key))
-            {
-                string str = ResourceLoader.GetForCurrentView(StringResourceName).GetString(StringResourceKey);
-                if (String.IsNullOrWhiteSpace(str))
-                    throw new NullReferenceException("No found title");
-
-                AllTitles.Add(key, str);
-            }
-
-            return AllTitles[key];
+            return str;
         }
     }
 }
