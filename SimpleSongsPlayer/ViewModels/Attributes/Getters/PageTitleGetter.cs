392e682979496f37c141fc3c9138ff38c923b8f4
李继龙 <kljzndx@outlook.com>
2018-12-18 10:02:35 +0800

    添加页面标题获取器

---
+++ SimpleSongsPlayer/ViewModels/Attributes/Getters/PageTitleGetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Windows.UI.Xaml.Controls;
+
+namespace SimpleSongsPlayer.ViewModels.Attributes.Getters
+{
+    public static class PageTitleGetter
+    {
+        private static readonly Dictionary<string, string> AllTitle = new Dictionary<string, string>();
+
+        public static string GetTitle(Type pageType)
+        {
+            if (!AllTitle.ContainsKey(pageType.FullName))
+            {
+                var attribute = pageType.GetTypeInfo().GetCustomAttribute<PageTitleAttribute>();
+                if (attribute is null)
+                    throw new Exception("No found title tag");
+                string title = attribute.GetTitle();
+
+                AllTitle.Add(pageType.FullName, title);
+            }
+
+            return AllTitle[pageType.FullName];
+        }
+    }
+}
