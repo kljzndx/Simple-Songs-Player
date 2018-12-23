17687093eb7eab32829c4b38d6390791164f1e7e
李继龙 <kljzndx@outlook.com>
2018-12-23 22:43:55 +0800

    Fixed bugs

--- SimpleSongsPlayer/ViewModels/Factories/MusicGroupers/MusicPathGrouper.cs
+++ SimpleSongsPlayer/ViewModels/Factories/MusicGroupers/MusicPathGrouper.cs
@@ -12,10 +12,10 @@ namespace SimpleSongsPlayer.ViewModels.Factories.MusicGroupers
         {
             var groups = source.GroupBy(fileDto =>
             {
-                List<string> relative = fileDto.FilePath.Replace(fileDto.LibraryFolderPath, String.Empty).Split('\\').ToList();
+                List<string> relativePaths = fileDto.FilePath.Replace(fileDto.LibraryFolderPath, String.Empty).Split('\\').ToList();
                 string folderName = fileDto.LibraryFolderPath.Split('\\').Last();
-                relative.Remove(relative.Last());
-                return $"{folderName}{String.Join("\\", relative)}";
+                relativePaths.Remove(relativePaths.Last());
+                return String.Format("{0}{1}", folderName, String.Join("\\", relativePaths));
             });
             foreach (var group in groups)
                 yield return new MusicFileGroup(group.Key, group);
