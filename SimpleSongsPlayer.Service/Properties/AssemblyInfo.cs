a4a6fdd247fc4c5d50a8f2d3b827e37be585a351
李继龙 <kljzndx@outlook.com>
2019-01-14 22:07:20 +0800

    为单元测试开辟一条通道

--- SimpleSongsPlayer.Service/Properties/AssemblyInfo.cs
+++ SimpleSongsPlayer.Service/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("SimpleSongsPlayer.Test")]
 
 // Version information for an assembly consists of the following four values:
 //
