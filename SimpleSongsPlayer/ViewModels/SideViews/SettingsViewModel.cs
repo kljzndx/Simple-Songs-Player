2e7014c615f021d6dd7623916b764a55f4898219
李继龙 <kljzndx@outlook.com>
2019-01-26 15:29:13 +0800

    删除垃圾代码

--- SimpleSongsPlayer/ViewModels/SideViews/SettingsViewModel.cs
+++ SimpleSongsPlayer/ViewModels/SideViews/SettingsViewModel.cs
@@ -1,26 +1,16 @@
-﻿using System;
-using System.Threading.Tasks;
-using Windows.Storage;
+﻿using Windows.Storage;
 using GalaSoft.MvvmLight;
-using SimpleSongsPlayer.ViewModels.DataServers;
 
 namespace SimpleSongsPlayer.ViewModels.SideViews
 {
     public class SettingsViewModel : ViewModelBase
     {
         private StorageLibrary musicLibrary;
-        private bool isSubmitting;
 
         public StorageLibrary MusicLibrary
         {
             get => musicLibrary;
             set => Set(ref musicLibrary, value);
-        }
-
-        public bool IsSubmitting
-        {
-            get => isSubmitting;
-            set => Set(ref isSubmitting, value);
         }
     }
 }
