e23dcbdaf4c8851773f7ac295088a7316e273b5b
李继龙 <kljzndx@outlook.com>
2019-02-05 15:02:10 +0800

    增加一个重载

--- SimpleSongsPlayer/ViewModels/Extensions/FrameExtension.cs
+++ SimpleSongsPlayer/ViewModels/Extensions/FrameExtension.cs
@@ -5,6 +5,11 @@ namespace SimpleSongsPlayer.ViewModels.Extensions
 {
     public static class FrameExtension
     {
+        public static void NavigateEx(this Frame frame, Type pageType)
+        {
+            if (frame.SourcePageType is null || frame.SourcePageType.FullName != pageType.FullName)
+                frame.Navigate(pageType);
+        }
         public static void NavigateEx(this Frame frame, Type pageType, object parameter)
         {
             if (frame.SourcePageType is null || frame.SourcePageType.FullName != pageType.FullName)
