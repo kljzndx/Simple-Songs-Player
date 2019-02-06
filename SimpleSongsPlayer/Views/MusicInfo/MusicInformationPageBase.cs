e26e414b691c17c24fe8a5d4a2d2eb1e343a76b1
李继龙 <kljzndx@outlook.com>
2019-02-06 08:51:57 +0800

    升级歌曲详情页

---
+++ SimpleSongsPlayer/Views/MusicInfo/MusicInformationPageBase.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using SimpleSongsPlayer.Models.DTO.Lyric;
+using SimpleSongsPlayer.ViewModels;
+using SimpleSongsPlayer.ViewModels.DataServers;
+using SimpleSongsPlayer.ViewModels.Events;
+using SimpleSongsPlayer.Views.Controllers;
+
+namespace SimpleSongsPlayer.Views.MusicInfo
+{
+    public class MusicInformationPageBase : Page
+    {
+        protected MusicInfoViewModel ViewModel;
+        private bool needRefreshLyric;
+
+        public MusicInformationPageBase()
+        {
+            CustomMediaPlayerElement.PositionChanged += CustomMediaPlayerElement_PositionChanged;
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+
+            LyricFileDataServer.Current.DataAdded += LyricFileDataServer_DataAdded;
+            LyricFileDataServer.Current.DataRemoved += LyricFileDataServer_DataRemoved;
+        }
+
+        protected void Init(MusicInfoViewModel vm)
+        {
+            this.ViewModel = vm;
+
+            vm.PropertyChanged += Vm_PropertyChanged;
+
+            GetControlByName<ListView>("LyricFile_ListView").ItemsSource = LyricFileDataServer.Current.Data;
+            GetControlByName<TextBlock>("NoFile_TextBlock").Visibility =
+                LyricFileDataServer.Current.Data.Any() ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private T GetControlByName<T>(string name)
+        {
+            var ctol = FindName(name);
+            if (ctol == null)
+                throw new ArgumentException($"No found control, Name: {name}");
+            if (!(ctol is T))
+                throw new ArgumentException($"Control Type error, Name: {name}");
+
+            return (T) ctol;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await ViewModel.Init();
+
+            GetControlByName<Grid>("LyricFileSelector_Grid").Visibility = Visibility.Collapsed;
+            GetControlByName<Grid>("LyricPreview_Grid").Visibility = Visibility.Visible;
+
+            var slfb = GetControlByName<Button>("SetUpLyricFile_Button");
+            slfb.Visibility = LyricIndexDataServer.Current.Data.Any() ? Visibility.Visible : Visibility.Collapsed;
+            slfb.IsEnabled = ViewModel.MusicSource != null;
+        }
+
+        private async void CustomMediaPlayerElement_PositionChanged(CustomMediaPlayerElement sender, PlayerPositionChangeEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var slp = GetControlByName<ScrollLyricsPreviewControl>("My_ScrollLyricsPreviewControl");
+                if (args.IsUser || needRefreshLyric)
+                {
+                    needRefreshLyric = false;
+                    slp.Reposition(args.Position);
+                }
+                else
+                    slp.RefreshLyric(args.Position);
+            });
+        }
+
+        private void LyricFileDataServer_DataAdded(object sender, IEnumerable<LyricFileDTO> e)
+        {
+            GetControlByName<TextBlock>("NoFile_TextBlock").Visibility = Visibility.Collapsed;
+        }
+
+        private void LyricFileDataServer_DataRemoved(object sender, IEnumerable<LyricFileDTO> e)
+        {
+            if (!LyricFileDataServer.Current.Data.Any())
+                GetControlByName<TextBlock>("NoFile_TextBlock").Visibility = Visibility.Collapsed;
+        }
+
+        protected void My_ScrollLyricsPreviewControl_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            CustomMediaPlayerElement.SetPosition_Global(((LyricLine)e.ClickedItem).Time);
+        }
+
+        private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ViewModel.LyricSource):
+                    needRefreshLyric = true;
+                    break;
+                case nameof(ViewModel.MusicSource):
+                    GetControlByName<Button>("SetUpLyricFile_Button").IsEnabled = ViewModel.MusicSource != null;
+                    break;
+            }
+        }
+
+        protected void SetUpLyricFile_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            GetControlByName<Grid>("LyricPreview_Grid").Visibility = Visibility.Collapsed;
+            GetControlByName<Grid>("LyricFileSelector_Grid").Visibility = Visibility.Visible;
+            var search = GetControlByName<TextBox>("Search_TextBox");
+            search.Text = String.Empty;
+
+            search.Focus(FocusState.Keyboard);
+        }
+
+        protected void Search_TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var lf = GetControlByName<ListView>("LyricFile_ListView");
+            var search = GetControlByName<TextBox>("Search_TextBox");
+            if (!String.IsNullOrWhiteSpace(search.Text))
+            {
+                var data = LyricFileDataServer.Current.Data.Where(f => f.FileName.Contains(search.Text)).ToList();
+                lf.ItemsSource = data;
+            }
+            else
+                lf.ItemsSource = LyricFileDataServer.Current.Data;
+        }
+
+        protected void Submit_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var lf = GetControlByName<ListView>("LyricFile_ListView");
+            LyricFileDTO lyric = lf.SelectedItem as LyricFileDTO;
+            if (ViewModel.MusicSource != null && lyric != null)
+                LyricIndexDataServer.Current.SetIndex(ViewModel.MusicSource.FilePath, lyric.FilePath);
+
+            GetControlByName<Grid>("LyricFileSelector_Grid").Visibility = Visibility.Collapsed;
+            GetControlByName<Grid>("LyricPreview_Grid").Visibility = Visibility.Visible;
+        }
+
+    }
+}
--- SimpleSongsPlayer/Views/MusicInformationPage.xaml.cs
+++ SimpleSongsPlayer/Views/MusicInformationPage.xaml.cs
@@ -20,7 +20,9 @@ using SimpleSongsPlayer.ViewModels;
 using SimpleSongsPlayer.ViewModels.Attributes;
 using SimpleSongsPlayer.ViewModels.DataServers;
 using SimpleSongsPlayer.ViewModels.Events;
+using SimpleSongsPlayer.ViewModels.Extensions;
 using SimpleSongsPlayer.Views.Controllers;
+using SimpleSongsPlayer.Views.MusicInfo;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -32,24 +34,11 @@ namespace SimpleSongsPlayer.Views
     [PageTitle("MusicInfoPage")]
     public sealed partial class MusicInformationPage : Page
     {
-        private MusicInfoViewModel vm;
-        private bool needRefreshLyric;
         private readonly ApplicationView _currentView = ApplicationView.GetForCurrentView();
 
         public MusicInformationPage()
         {
             this.InitializeComponent();
-            vm = DataContext as MusicInfoViewModel;
-            vm.PropertyChanged += Vm_PropertyChanged;
-            CustomMediaPlayerElement.PositionChanged += CustomMediaPlayerElement_PositionChanged;
-            NavigationCacheMode = NavigationCacheMode.Enabled;
-
-            LyricFile_ListView.ItemsSource = LyricFileDataServer.Current.Data;
-            NoFile_TextBlock.Visibility =
-                LyricFileDataServer.Current.Data.Any() ? Visibility.Collapsed : Visibility.Visible;
-            LyricFileDataServer.Current.DataAdded += LyricFileDataServer_DataAdded;
-            LyricFileDataServer.Current.DataRemoved += LyricFileDataServer_DataRemoved;
-
             if (!ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationViewMode") ||
                 !ApiInformation.IsEnumNamedValuePresent(typeof(ApplicationViewMode).FullName, "CompactOverlay") ||
                 !_currentView.IsViewModeSupported(ApplicationViewMode.CompactOverlay))
@@ -60,96 +49,19 @@ namespace SimpleSongsPlayer.Views
 
         private void AutoScale()
         {
-            MusicInfo_StackPanel.Visibility = ActualHeight > 500 ? Visibility.Visible : Visibility.Collapsed;
+            if (ActualWidth > 800)
+                Main_Frame.NavigateEx(typeof(MusicInformationLongPage));
+            else
+                Main_Frame.NavigateEx(typeof(MusicInformationSmallPage));
+            Main_Frame.BackStack.Clear();
         }
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            await vm.Init();
-
-            LyricFileSelector_Grid.Visibility = Visibility.Collapsed;
-            LyricPreview_Grid.Visibility = Visibility.Visible;
-
-            SetUpLyricFile_Button.Visibility = LyricIndexDataServer.Current.Data.Any() ? Visibility.Visible : Visibility.Collapsed;
-            SetUpLyricFile_Button.IsEnabled = vm.MusicSource != null;
-
             AutoScale();
         }
-
-        private async void CustomMediaPlayerElement_PositionChanged(CustomMediaPlayerElement sender, PlayerPositionChangeEventArgs args)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                if (args.IsUser || needRefreshLyric)
-                {
-                    needRefreshLyric = false;
-                    My_ScrollLyricsPreviewControl.Reposition(args.Position);
-                }
-                else
-                    My_ScrollLyricsPreviewControl.RefreshLyric(args.Position);
-            });
-        }
-
-        private void LyricFileDataServer_DataAdded(object sender, IEnumerable<LyricFileDTO> e)
-        {
-            NoFile_TextBlock.Visibility = Visibility.Collapsed;
-        }
-
-        private void LyricFileDataServer_DataRemoved(object sender, IEnumerable<LyricFileDTO> e)
-        {
-            if (!LyricFileDataServer.Current.Data.Any())
-                NoFile_TextBlock.Visibility = Visibility.Collapsed;
-        }
-
-        private void My_ScrollLyricsPreviewControl_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            CustomMediaPlayerElement.SetPosition_Global(((LyricLine) e.ClickedItem).Time);
-        }
-
-        private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            switch (e.PropertyName)
-            {
-                case nameof(vm.LyricSource):
-                    needRefreshLyric = true;
-                    break;
-                case nameof(vm.MusicSource):
-                    SetUpLyricFile_Button.IsEnabled = vm.MusicSource != null;
-                    break;
-            }
-        }
-
-        private void SetUpLyricFile_Button_OnClick(object sender, RoutedEventArgs e)
-        {
-            LyricPreview_Grid.Visibility = Visibility.Collapsed;
-            LyricFileSelector_Grid.Visibility = Visibility.Visible;
-            Search_TextBox.Text = String.Empty;
-
-            Search_TextBox.Focus(FocusState.Keyboard);
-        }
-
-        private void Search_TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (!String.IsNullOrWhiteSpace(Search_TextBox.Text))
-            {
-                var data = LyricFileDataServer.Current.Data.Where(f => f.FileName.Contains(Search_TextBox.Text)).ToList();
-                LyricFile_ListView.ItemsSource = data;
-            }
-            else
-                LyricFile_ListView.ItemsSource = LyricFileDataServer.Current.Data;
-        }
-
-        private void Submit_Button_OnClick(object sender, RoutedEventArgs e)
-        {
-            LyricFileDTO lyric = LyricFile_ListView.SelectedItem as LyricFileDTO;
-            if (vm.MusicSource != null && lyric != null)
-                LyricIndexDataServer.Current.SetIndex(vm.MusicSource.FilePath, lyric.FilePath);
-
-            LyricFileSelector_Grid.Visibility = Visibility.Collapsed;
-            LyricPreview_Grid.Visibility = Visibility.Visible;
-        }
-
+        
         private void MusicInformationPage_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
             AutoScale();
