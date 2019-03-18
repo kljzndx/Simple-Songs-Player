49b7bbfc699d5bf02988eb393f9daf3c22ade40a
李继龙 <kljzndx@outlook.com>
2019-03-18 17:23:30 +0800

    Fixed bugs

---
+++ SimpleSongsPlayer/ViewModels/Converters/BoolToVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleSongsPlayer.ViewModels.Converters
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is null)
+                return Visibility.Collapsed;
+
+            return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is null)
+                return false;
+
+            return (Visibility) value == Visibility.Visible;
+        }
+    }
+}
