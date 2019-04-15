a0d2cdffe83a72c3c943e66cf1ac91b4e5596286
李继龙 <kljzndx@outlook.com>
2019-04-15 13:09:41 +0800

    给播放项添加id列

--- SimpleSongsPlayer.DAL/Migrations/20190414110242_AddIdColumn.Designer.cs
+++ SimpleSongsPlayer.DAL/Migrations/20190414110242_AddIdColumn.Designer.cs
@@ -8,9 +8,10 @@ using SimpleSongsPlayer.DAL;
 namespace SimpleSongsPlayer.DAL.Migrations
 {
     [DbContext(typeof(FilesContext))]
-    partial class FilesContextModelSnapshot : ModelSnapshot
+    [Migration("20190414110242_AddIdColumn")]
+    partial class AddIdColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.6");
--- SimpleSongsPlayer.DAL/PlaybackItem.cs
+++ SimpleSongsPlayer.DAL/PlaybackItem.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SimpleSongsPlayer.DAL
 {
@@ -10,19 +12,42 @@ namespace SimpleSongsPlayer.DAL
 
     public class PlaybackItem
     {
+        private static ulong AllItemCount = 0;
+
+        private ulong index;
+
         public PlaybackItem()
         {
+
         }
 
         public PlaybackItem(string path, FileSourceMembers source)
         {
+            Index = AllItemCount + 1;
             Path = path;
             Source = source;
+        }
+        
+        public ulong Index
+        {
+            get => index;
+            set
+            {
+                index = value;
+
+                if (value > AllItemCount)
+                    AllItemCount = value;
+            }
         }
 
         [Key]
         public string Path { get; set; }
 
         public FileSourceMembers Source { get; set; }
+
+        public static void ResetCount()
+        {
+            AllItemCount = 0;
+        }
     }
 }
