4616046c22b2c151d822404a16a1c6cc14ca84b9
李继龙 <kljzndx@outlook.com>
2022-03-02 08:48:44 +0800

    设计 MusicFile 表

--- SimpleSongsPlayer.Dal/MainContext.cs
+++ SimpleSongsPlayer.Dal/MainContext.cs
@@ -4,6 +4,17 @@ namespace SimpleSongsPlayer.Dal
 {
     public class MainContext : DbContext
     {
+        public DbSet<MusicFile> MusicFiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MusicFile>().HasIndex(mf => mf.FilePath);
+            modelBuilder.Entity<MusicFile>().HasIndex(mf => mf.LibraryFolder);
+            modelBuilder.Entity<MusicFile>().HasIndex(mf => mf.IsInPlaybackList);
+
+            modelBuilder.Entity<MusicFile>().Property(mf => mf.DbVersion).HasDefaultValue("V1");
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=Main.db");
---
+++ SimpleSongsPlayer.Dal/MusicFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleSongsPlayer.Dal
+{
+    public class MusicFile
+    {
+        public MusicFile() { }
+
+        public MusicFile(string title, string artist, string album, TimeSpan duration
+                        , string filePath, string libraryFolder, DateTime fileChangeDate)
+        {
+            Title = title;
+            Artist = artist;
+            Album = album;
+            Duration = duration;
+
+            FilePath = filePath;
+            LibraryFolder = libraryFolder;
+            FileChangeDate = fileChangeDate;
+        }
+
+        [Key]
+        public int Index { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string FilePath { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string LibraryFolder { get; set; }
+        public DateTime FileChangeDate { get; set; }
+
+        public bool IsInPlaybackList { get; set; }
+
+        public string DbVersion { get; set; }
+    }
+}
