7ab1cc19644d89795827e3bba95feed192224022
李继龙 <kljzndx@outlook.com>
2022-03-02 15:20:09 +0800

    编写 MusicGroup 类

---
+++ SimpleSongsPlayer/Models/MusicGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicGroup
+    {
+        public MusicGroup(string name, IEnumerable<MusicUi> items)
+        {
+            Name = name;
+            Items = items;
+        }
+
+        public string Name { get; }
+        public IEnumerable<MusicUi> Items { get; }
+    }
+}
