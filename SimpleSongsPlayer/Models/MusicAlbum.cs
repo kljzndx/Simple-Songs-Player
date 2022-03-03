5dc71bc8fced9f9db8f60667bb328225892a863c
李继龙 <kljzndx@outlook.com>
2022-03-03 12:39:30 +0800

    编写 MusicAlbum 类

---
+++ SimpleSongsPlayer/Models/MusicAlbum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SimpleSongsPlayer.Models
+{
+    public class MusicAlbum : MusicGroup
+    {
+        public MusicAlbum(string name, IEnumerable<MusicUi> items) : base(name, items)
+        {
+        }
+
+        public int Count => Items.Count();
+
+        public Task<BitmapSource> GetCover()
+        {
+            return Items.First().GetCover();
+        }
+    }
+}
