db8f5041b0b62be181ad659404909f08e75becff
李继龙 <kljzndx@outlook.com>
2022-03-04 16:00:19 +0800

    初步完成编写 MusicListViewModel 类

---
+++ SimpleSongsPlayer/ViewModels/MusicListViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using SimpleSongsPlayer.Models;
+using SimpleSongsPlayer.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongsPlayer.ViewModels
+{
+    public class MusicListViewModel : ObservableRecipient
+    {
+        private Func<MusicFileManageService, List<MusicGroup>> _sourceGetter;
+        private MusicFileManageService _manageService;
+
+        private List<MusicGroup> _source;
+
+        public MusicListViewModel(MusicFileManageService manageService)
+        {
+            _manageService = manageService;
+        }
+
+        public List<MusicGroup> Source
+        {
+            get => _source;
+            set => SetProperty(ref _source, value);
+        }
+
+        public void Load(Func<MusicFileManageService, List<MusicGroup>> sourceGetter)
+        {
+            this._sourceGetter = sourceGetter;
+            Source = _sourceGetter.Invoke(_manageService);
+        }
+
+        public void Refresh()
+        {
+            Source = _sourceGetter.Invoke(_manageService);
+        }
+    }
+}
