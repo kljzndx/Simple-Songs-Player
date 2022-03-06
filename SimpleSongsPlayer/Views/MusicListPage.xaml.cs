677fde5e0a0b29eaa5b12164b79e21a728a9c426
李继龙 <kljzndx@outlook.com>
2022-03-06 16:11:41 +0800

    编写 MusicListPage 页面

---
+++ SimpleSongsPlayer/Views/MusicListPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
+
+using SimpleSongsPlayer.Services;
+using SimpleSongsPlayer.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace SimpleSongsPlayer.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MusicListPage : Page
+    {
+        private MusicListViewModel _vm;
+
+        public MusicListPage()
+        {
+            this.InitializeComponent();
+            _vm = Ioc.Default.GetRequiredService<MusicListViewModel>();
+        }
+    }
+}
