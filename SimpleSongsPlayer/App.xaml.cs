8aebb2d4b23422b428b6f17dfb8ae13a656efd6a
李继龙 <kljzndx@outlook.com>
2022-03-07 14:58:57 +0800

    注入 MediaPlayer 类

--- SimpleSongsPlayer/App.xaml.cs
+++ SimpleSongsPlayer/App.xaml.cs
@@ -17,6 +17,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -58,6 +59,8 @@ namespace SimpleSongsPlayer
                 })
                 .AddScoped<MusicFileManageService>()
                 .AddScoped<MusicFileScanningService>()
+
+                .AddSingleton<MediaPlayer>()
 
                 .AddSingleton<MainViewModel>()
                 .AddSingleton<MusicListViewModel>()
