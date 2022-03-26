21593b6c68afe1d7e97f240c264bbecd47b59103
李继龙 <kljzndx@outlook.com>
2022-03-26 19:42:38 +0800

    初步完成通知控件编写

---
+++ SimpleSongsPlayer/Views/Controllers/FlyoutNotification.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace SimpleSongsPlayer.Views.Controllers
+{
+    public sealed partial class FlyoutNotification : UserControl
+    {
+        public FlyoutNotification()
+        {
+            this.InitializeComponent();
+        }
+
+        public void Show(string message, TimeSpan timeout)
+        {
+            Show(message);
+
+            ThreadPoolTimer.CreateTimer(timer => Hide(), timeout);
+        }
+
+        public void Show(string message)
+        {
+            Message_TextBlock.Text = message;
+            Root_Border.Visibility = Visibility.Visible;
+            Extend_Storyboard.Begin();
+        }
+
+        public void Hide()
+        {
+            Fold_Storyboard.Begin();
+        }
+
+        private void Fold_Storyboard_Completed(object sender, object e)
+        {
+            Root_Border.Visibility = Visibility.Collapsed;
+        }
+    }
+}
