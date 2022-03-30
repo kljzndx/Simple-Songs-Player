bdfc2795c773feed159f9c336fdab7f0bbf6f982
李继龙 <kljzndx@outlook.com>
2022-03-30 20:18:57 +0800

    将音乐信息展示部分抽离成一个控件

---
+++ SimpleSongsPlayer/Views/Controllers/MusicInfoDisplay.xaml.cs
@@ -0,0 +1,70 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
+
+using HappyStudio.Subtitle.Control.Interface.Models;
+
+using SimpleSongsPlayer.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace SimpleSongsPlayer.Views.Controllers
+{
+    public sealed partial class MusicInfoDisplay : UserControl
+    {
+        private MusicInfoViewModel _vm;
+
+        public MusicInfoDisplay()
+        {
+            this.InitializeComponent();
+            _vm = Ioc.Default.GetRequiredService<MusicInfoViewModel>();
+
+            WeakReferenceMessenger.Default.Register<MusicInfoDisplay, string, string>(this, "MediaPlayer", (ctor, mes) =>
+            {
+                string[] split = mes.Split(':');
+                string key = split[0];
+                string value = split[1];
+
+                var time = TimeSpan.FromMinutes(double.Parse(value));
+                if (key == "PositionChangedBySystem")
+                    ctor.MyScrollSubtitlePreview.Refresh(time);
+                if (key == "PositionChangedByUser")
+                    ctor.MyScrollSubtitlePreview.Reposition(time);
+            });
+
+            WeakReferenceMessenger.Default.Register<MusicInfoDisplay, string, string>(this, nameof(MusicInfoViewModel), (ctor, mes) =>
+            {
+                if (mes == "SubtitleLoaded")
+                {
+                    var player = Ioc.Default.GetRequiredService<MediaPlayer>();
+                    ctor.MyScrollSubtitlePreview.Reposition(player.PlaybackSession.Position);
+                }
+            });
+        }
+
+        private void MyScrollSubtitlePreview_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as SubtitleLineUi;
+            if (item == null)
+                return;
+
+            WeakReferenceMessenger.Default.Send($"RequestChangePosition:{item.StartTime.TotalMinutes}", "MediaPlayer");
+        }
+    }
+}
--- SimpleSongsPlayer/Views/MusicInfoPage.xaml.cs
+++ SimpleSongsPlayer/Views/MusicInfoPage.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
-using CommunityToolkit.Mvvm.Messaging;
-
-using HappyStudio.Subtitle.Control.Interface.Models;
 
 using SimpleSongsPlayer.ViewModels;
 
@@ -37,46 +34,12 @@ namespace SimpleSongsPlayer.Views
         {
             this.InitializeComponent();
             _vm = Ioc.Default.GetRequiredService<MusicInfoViewModel>();
-
-            WeakReferenceMessenger.Default.Register<MusicInfoPage, string, string>(this, "MediaPlayer", (page, mes) =>
-            {
-                string[] split = mes.Split(':');
-                string key = split[0];
-                string value = split[1];
-
-                var time = TimeSpan.FromMinutes(double.Parse(value));
-                if (key == "PositionChangedBySystem")
-                    page.MyScrollSubtitlePreview.Refresh(time);
-                if (key == "PositionChangedByUser")
-                    page.MyScrollSubtitlePreview.Reposition(time);
-            });
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-
-            var player = Ioc.Default.GetRequiredService<MediaPlayer>();
-            MyScrollSubtitlePreview.Reposition(player.PlaybackSession.Position);
-        }
-
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
-        {
             await _vm.AutoLoad();
-        }
-
-        private void Back_Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.GoBack();
-        }
-
-        private void MyScrollSubtitlePreview_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var item = e.ClickedItem as SubtitleLineUi;
-            if (item == null)
-                return;
-
-            WeakReferenceMessenger.Default.Send($"RequestChangePosition:{item.StartTime.TotalMinutes}", "MediaPlayer");
         }
     }
 }
