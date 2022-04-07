cf9b9740deb0d323f13ea215f7a6e32ee16c9f18
李继龙 <kljzndx@outlook.com>
2022-04-07 08:25:10 +0800

    Revert "再次尝试解决进度条无法获取音乐时长问题"
    This reverts commit 82f4eb0179f13e7cb79b6b43a281b91fb9d20978.

--- SimpleSongsPlayer/Views/Controllers/CustomPlayerElement.xaml.cs
+++ SimpleSongsPlayer/Views/Controllers/CustomPlayerElement.xaml.cs
@@ -175,19 +175,21 @@ namespace SimpleSongsPlayer.Views.Controllers
         {
             await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                switch (sender.PlaybackState)
-                {
-                    case MediaPlaybackState.Opening:
-                    case MediaPlaybackState.Buffering:
-                    case MediaPlaybackState.Playing:
-                        Position_Slider.Maximum = sender.NaturalDuration.TotalMinutes;
-                        sender.PlaybackRate = _configService.PlaybackRate;
-                        break;
-                    case MediaPlaybackState.Paused:
-                    case MediaPlaybackState.None:
-                    default:
-                        break;
-                }
+                //switch (sender.PlaybackState)
+                //{
+                //    case MediaPlaybackState.Opening:
+                //    case MediaPlaybackState.Buffering:
+                //    case MediaPlaybackState.Playing:
+                //        Play_Button.Visibility = Visibility.Collapsed;
+                //        Pause_Button.Visibility = Visibility.Visible;
+                //        break;
+                //    case MediaPlaybackState.Paused:
+                //    case MediaPlaybackState.None:
+                //    default:
+                //        Play_Button.Visibility = Visibility.Visible;
+                //        Pause_Button.Visibility = Visibility.Collapsed;
+                //        break;
+                //}
 
                 PlayOrPause_ToggleButton.IsChecked = sender.PlaybackState == MediaPlaybackState.Playing;
             });
